bfafc9a5f896695f684a903e22f836324062be76
Kushal <am.kushal02@gmail.com>
2024-04-04 23:54:20 +0530

    Delete cpp directory

--- cpp/1290_decimal_from_binary_list.cpp
+++
@@ -1,9 +0,0 @@
-int getDecimalValue(ListNode* head) {
-        int ans = 0;
-        while(head){
-            ans = ans*2 + head->val;
-            head = head->next;
-        }
-        
-        return ans;
-    }
