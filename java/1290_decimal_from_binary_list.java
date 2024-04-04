0d3ff844c611caa6f56e1f9f9ac9fdac0cafae7f
Kushal <am.kushal02@gmail.com>
2024-04-05 00:00:29 +0530

    Delete java directory

--- java/1290_decimal_from_binary_list.java
+++
@@ -1,8 +0,0 @@
-public int getDecimalValue(ListNode head) {
-        int ans = 0;
-        while(head!=null){
-            ans = ans*2 + head.val;
-            head = head.next;
-        }
-        return ans;
-    }
