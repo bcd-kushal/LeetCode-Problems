26a1908f693622f818a80c0a6ade7b0fdba49323
Kushal <am.kushal02@gmail.com>
2024-04-13 23:46:29 +0530

    Update 0020_valid_paranthesis.cpp

--- cpp/easy/0020_valid_paranthesis.cpp
+++ cpp/easy/0020_valid_paranthesis.cpp
@@ -6,7 +6,7 @@ public:
     bool isValid(string s) {
         stack<char> st;
         int n = s.size(); 
-
+ 
         for(int i=0; i<n; i++) {
             if(s[i]=='(' || s[i]=='{' || s[i]=='[')
                 st.push(s[i]);
