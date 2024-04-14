7522dacb2e1fbef383b7d35b07da0515df45ca03
Kushal <am.kushal02@gmail.com>
2024-04-14 23:59:22 +0530

    Update 0020_valid_paranthesis.cpp

--- cpp/easy/0020_valid_paranthesis.cpp
+++ cpp/easy/0020_valid_paranthesis.cpp
@@ -8,7 +8,7 @@ public:
         int n = s.size();   
   
         for(int i=0; i<n; i++) { 
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[') 
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')  
                 st.push(s[i]); 
             else {
                 if(st.empty())   
