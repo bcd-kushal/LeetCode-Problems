3b5c890c1442b9c5078aa3de94ec40a9ca9cf9cc
Kushal <am.kushal02@gmail.com>
2024-05-06 23:59:34 +0530

    Update 392_isSubsequence.py

--- py/392_isSubsequence.py
+++ py/392_isSubsequence.py
@@ -7,7 +7,7 @@ def isSubsequence(s:str, t:str)->bool:
     
     i:int = 0
     for j in range(len(t)):
-        if t[j]==s[i]:
+        if t[j]==s[i]: 
             i+=1
 
     if i<len(s) and i==len(s):
