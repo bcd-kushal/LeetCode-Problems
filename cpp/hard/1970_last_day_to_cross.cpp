810b788a326f544cbb6864123e8ecd224dcc245d
Kushal <am.kushal02@gmail.com>
2024-07-01 10:34:34 +0530

    Update 1970_last_day_to_cross.cpp

--- cpp/hard/1970_last_day_to_cross.cpp
+++ cpp/hard/1970_last_day_to_cross.cpp
@@ -5,60 +5,63 @@ class LastDay {
 private:
     bool f(int row,int col,int mid,vector<vector<int>> &cells){
         vector<vector<int>>grid(row,vector<int>(col));
-        for(int i = 0;i < mid;i++){
+        for(int i = 0;i < mid;i++)
             grid[cells[i][0]-1][cells[i][1]-1] = 1;
-        }
+        
         vector<vector<int>>Visited(row,vector<int>(col));
         queue<pair<int,int>>Q;
-        for(int i = 0;i < col;i++){
-            if(grid[0][i] == 0){
+        
+        for(int i = 0;i < col;i++)
+            if(grid[0][i] == 0)
                 Q.push({0,i});
-            }
-        }
+            
         vector<int>dirx = {1,-1,0,0};
         vector<int>diry = {0,0,1,-1};
+        
         while(!Q.empty()){
             int x = Q.front().first;
             int y = Q.front().second;
             Q.pop();
+            
             if(x == row-1)return true;
+            
             for(int  i = 0;i < 4;i++){
                 int tempx = x + dirx[i];
                 int tempy = y + diry[i];
-                if(tempx >= 0 && tempy >= 0 && tempx < row && tempy < col){
+                if(tempx >= 0 && tempy >= 0 && tempx < row && tempy < col)
                     if(Visited[tempx][tempy] == 0 && grid[tempx][tempy] == 0){
                         Q.push({tempx,tempy});
                         if(tempx == row-1)return true;
                         Visited[tempx][tempy] = 1;
                     }
-                }
+                
             }
         }
+        
         return false;
     }
 
     void resetVector(vector<vector<int>> &grid){
-        for(int i = 0;i < grid.size();i++){
-            for(int j = 0;j < grid[i].size();j++){
+        for(int i = 0;i < grid.size();i++)
+            for(int j = 0;j < grid[i].size();j++)
                 grid[i][j] = 0;
-            }
-        }
     }
     
 public:
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
         int low = 0,high = cells.size();
         int last_day_to_cross = 0;
+        
         while(low <= high){
             int mid = (high + low) / 2;
             if(f(row,col,mid,cells)){
                 low = mid + 1;
                 last_day_to_cross = max(last_day_to_cross,mid);
             }
-            else{
+            else
                 high = mid - 1;
-            }
         }
+        
         return last_day_to_cross;
     }
 };
@@ -73,8 +76,6 @@ int main() {
     auto end = chrono::steady_clock::now();
 
     cout << "Last day to cross: " << result << endl;
-
-
     cout << "...time taken: " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << "ms";
 
     return 0;
