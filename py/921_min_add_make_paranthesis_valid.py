08f1c505015646e4780eebae3c25aac1761cf681
Kushal <am.kushal02@gmail.com>
2024-07-04 10:18:31 +0530

    Update 921_min_add_make_paranthesis_valid.py

--- py/921_min_add_make_paranthesis_valid.py
+++ py/921_min_add_make_paranthesis_valid.py
@@ -1,14 +1,14 @@
 def minAddToMakeValid(self, s: str) -> int:
-        lc:int = 0
-        extra:int = 0
+        lc: int = 0
+        extra: int = 0
         
         for i in range(len(s)):
-            if s[i]=='(':
-                lc+=1
+            if s[i] == '(':
+                lc += 1
             else:
-                if lc>0:
-                    lc-=1
+                if lc > 0:
+                    lc -= 1
                 else:
-                    extra+=1
+                    extra += 1
                     
-        return extra+lc
+        return extra + lc
