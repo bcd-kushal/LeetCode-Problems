cbdcb030e02aff50139bf462b721590be823c91c
Kushal Kumar <dev@kushalkumarsaha.com>
2024-09-10 14:47:32 +0530

    84. largest rectangle in histogram

---
+++ hard/0084-largest-rectangle-in-histogram/sol.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int maxArea = INT_MIN;
+        int local_min = INT_MAX;
+        int len = heights.size();
+
+        for(int i=0;i<len;i++) {
+            local_min = heights[i];
+            for(int j=i;j<len;j++) {
+                int l = j-i+1;
+                int b = min(heights[j],local_min);
+                local_min = b;
+
+                maxArea = max(maxArea, l*b);
+            }
+        }
+
+        return maxArea;
+    }
+};
