92f8c5c91736d1b554d4306f15236048b0036518
Kushal Kumar <dev@kushalkumarsaha.com>
2024-09-10 11:17:30 +0530

    42. trapping rain water

---
+++ hard/0042-trapping-rain-water/sol.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int len = height.size();
+        vector<int> left(len), 
+                    right(len);
+        int waterTrapped = 0;
+
+        for(int i=0, j=len-1; i < len, j >= 0; i++, j--) {
+            int l = i==0 ? INT_MIN : left[i-1];
+            left[i] = max(l ,height[i]);
+
+            int r = j==len-1 ? INT_MIN : right[j+1];
+            right[j] = max(r, height[j]);
+        }
+
+        for(int i=0; i<len; i++) 
+            waterTrapped += min(left[i],right[i]) - height[i];
+        
+        return waterTrapped;
+    }
+};
